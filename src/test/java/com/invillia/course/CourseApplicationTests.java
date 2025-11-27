b5b3ad432d513cd37f18ff638bc66df93a4502e6
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-11-27 11:21:51 -0300

    project created

---
+++ src/test/java/com/invillia/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
