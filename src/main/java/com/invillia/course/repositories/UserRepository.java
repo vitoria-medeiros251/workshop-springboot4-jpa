eaad2dd00ab185cc2462039300160f959c681b18
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-11-28 14:34:25 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/invillia/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.invillia.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.invillia.course.entities.User;
+
+public interface  UserRepository extends JpaRepository <User,Long> {
+
+}
