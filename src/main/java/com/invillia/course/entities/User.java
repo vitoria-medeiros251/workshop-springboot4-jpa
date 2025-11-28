1a2ffc8c6f15690005bc6f347b038e2977c9b805
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-11-28 11:42:57 -0300

    H2 database, test profile, JPA

--- src/main/java/com/invillia/course/entities/User.java
+++ src/main/java/com/invillia/course/entities/User.java
@@ -3,15 +3,26 @@ package com.invillia.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	 
+
 	public User() {
 	}
 
@@ -80,5 +91,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-}    
+
+}
