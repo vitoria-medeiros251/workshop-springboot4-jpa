9c0593913c3b9ff061748c1f600921d08337b957
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-12-01 13:32:31 -0300

    service layer,componente registration

--- src/main/java/com/invillia/course/repositories/UserRepository.java
+++ src/main/java/com/invillia/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.invillia.course.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.invillia.course.entities.User;
 
 public interface  UserRepository extends JpaRepository <User,Long> {
 
+	
 }
---
+++ src/main/java/com/invillia/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.invillia.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+
+import com.invillia.course.entities.User;
+import com.invillia.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+   public User findyById(long id) {
+   Optional<User> obj = repository.findById(id);
+   return obj.get();
+  }
+
+}
