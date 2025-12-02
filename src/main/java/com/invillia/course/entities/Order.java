f7f01018e38bf8059e0494cf51bd0f482ec9517e
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-12-02 12:30:56 -0300

    OrderStatus enum

--- src/main/java/com/invillia/course/entities/Order.java
+++ src/main/java/com/invillia/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.invillia.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -33,6 +34,9 @@ public class Order implements Serializable{
 	private Instant moment;
 	
 	
+	private Integer orderStatus;
+	
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -42,10 +46,11 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus , User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -63,6 +68,18 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null ) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
