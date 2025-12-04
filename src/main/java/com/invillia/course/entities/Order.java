500b73ccb06e3de1759984a7105c1d8af5e15e5e
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-12-04 11:39:00 -0300

    Payment, one-to-one association

--- src/main/java/com/invillia/course/entities/Order.java
+++ src/main/java/com/invillia/course/entities/Order.java
@@ -6,6 +6,10 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+
+import jakarta.persistence.CascadeType;
+
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.invillia.course.entities.enums.OrderStatus;
 
@@ -16,6 +20,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -45,6 +50,10 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy= "order", cascade = CascadeType.ALL)
+	
+	private Payment payment;
 
 	
 	public Order() {
@@ -95,7 +104,16 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
-    public Set<OrderItem> getItems(){
+	
+    public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems(){
     	return items;
     }
 	@Override
